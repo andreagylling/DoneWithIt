fe1f373900f602feed223f5ba0aebe848edba78c
Andrea Gylling <andrea.gylling@appliedtechnology.se>
2020-08-14 14:12:16 +0200

    fixed loading flicker and added custom splash

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import jwtDecode from "jwt-decode";
+import { AppLoading } from "expo";
 import { AuthNavigator, AppNavigator } from "./app/navigation";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
@@ -8,17 +9,21 @@ import authStorage from "./app/auth/storage";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [ready, setReady] = useState(false);
 
   const restoreAuthToken = async () => {
     const authToken = await authStorage.getToken();
-    console.log("authToken from store", authToken);
     if (!authToken) return;
     setUser(jwtDecode(authToken));
   };
 
-  useEffect(() => {
-    restoreAuthToken();
-  }, []);
+  if (!ready)
+    return (
+      <AppLoading
+        startAsync={restoreAuthToken}
+        onFinish={() => setReady(true)}
+      />
+    );
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
